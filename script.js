887692d2f9816c520e79804776f481bd81b81abb
horobox <horometer@protonmail.com>
2020-06-06 14:27:35 +0530

    Episode 5

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ const ctx = canvas.getContext('2d');
 
 const scale = 20;
 
+const rows = canvas.height/scale;
+const columns = canvas.width/scale;
+
+var snake;
+var food;
 (function setup(){
     snake = new Snake();
-    
+	food = new Food();
+	food.pickLocation();
+
 	window.setInterval(() => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         snake.update();
 		snake.draw();
+		food.draw();
 	}, 180);
 }());
 
